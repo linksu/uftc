9f9d94e468c677a23d0ac390b2c39fadebbe903c
Miia V <naminomppis@hotmail.com>
2013-02-08 10:36:38 +0200

    Käyttöoikeuksien tarkistuksia tehty controllerissa

--- src/main/java/net/ambientia/uftc/controller/ChallengeSportEventsController.java
+++ src/main/java/net/ambientia/uftc/controller/ChallengeSportEventsController.java
@@ -24,105 +24,128 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ChallengeSportEventsController {
-	
+
 	@Autowired
 	private ChallengeSportEventService challengeSportEventService;
-	
+
 	@Autowired
 	private ChallengeService challengeService;
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	private static final Logger logger = LoggerFactory
 			.getLogger(ChallengeSportEventsController.class);
 
-	
 	@RequestMapping(value = "/challengeSportEvent/show", method = RequestMethod.GET)
-	public String show(@RequestParam("challengeId") int challengeId, Model model, Principal principal) {
+	public String show(@RequestParam("challengeId") int challengeId,
+			Model model, Principal principal) {
 		logger.debug("Received request to show list challenge's sportevents page");
-		
+
+		Challenge challenge = challengeService.getById(challengeId);
 		User currentUser = userService.getUserByUsername(principal.getName());
-		
-		List<ChallengeSportEvent> events = challengeSportEventService.getAllByChallengeId(challengeId);
-		
+
+		if (!currentUser.getId().equals(challenge.getOwner().getId())) {
+			// Attempted to edit wrong challenge data
+			return "redirect:/denied";
+		}
+
+		List<ChallengeSportEvent> events = challengeSportEventService
+				.getAllByChallengeId(challengeId);
+
 		model.addAttribute("challengeSportEvents", events);
 		model.addAttribute("challengeId", challengeId);
 		model.addAttribute("loggedInUser", currentUser);
 		return "challengeSportEvent/show";
 	}
-	
+
 	@RequestMapping(value = "/challengeSportEvent/add", method = RequestMethod.GET)
-	public String showAddNew(@RequestParam("challengeId") int challengeId, Model model, Principal principal) {
+	public String showAddNew(@RequestParam("challengeId") int challengeId,
+			Model model, Principal principal) {
 		logger.debug("Received request to show list challenge's sportevents page");
+
+		Challenge challenge = challengeService.getById(challengeId);
 		User currentUser = userService.getUserByUsername(principal.getName());
-				
-		model.addAttribute("challengeSportEventInstance", new ChallengeSportEvent());
+
+		if (!currentUser.getId().equals(challenge.getOwner().getId())) {
+			// Attempted to edit wrong challenge data
+			return "redirect:/denied";
+		}
+
+		model.addAttribute("challengeSportEventInstance",
+				new ChallengeSportEvent());
 		model.addAttribute("pointFactorTypeEnum", PointFactorType.values());
 		model.addAttribute("challengeId", challengeId);
 		model.addAttribute("loggedInUser", currentUser);
-		
+
 		return "challengeSportEvent/add";
 	}
-	
+
 	@RequestMapping(value = "/challengeSportEvent/add", method = RequestMethod.POST)
-	public String addSport(@ModelAttribute("challengeSportEventInstance") ChallengeSportEvent challengeSportEvent, 
-			@RequestParam("challengeId") int challengeId,
-			Model model, Principal principal) {
+	public String addSport(
+			@ModelAttribute("challengeSportEventInstance") ChallengeSportEvent challengeSportEvent,
+			@RequestParam("challengeId") int challengeId, Model model,
+			Principal principal) {
 		logger.debug("Received request to add new challenge");
-		
-		String currentUser = principal.getName();	
-		
-		
-		challengeSportEventService.add(challengeId, challengeSportEvent);
-		
-//		if (challengeService.isValid(challenge)) {
-//			challengeService.add(challenge);
-//			return "redirect:/challenge/list";
-//
-//		} else {
-//			model.addAttribute("challengeInstance", challenge);
-//			model.addAttribute("errors",
-//					challengeService.getValidationErrorList(challenge));
-//			return "challenge/add";
-//
-//		}
+
+		Challenge challenge = challengeService.getById(challengeId);
+		User currentUser = userService.getUserByUsername(principal.getName());
+
+		if (!currentUser.getId().equals(challenge.getOwner().getId())) {
+			// Attempted to edit wrong challenge data
+			return "redirect:/denied";
+		}
+
+		if (challenge != null) {
+			challengeSportEventService.add(challengeId, challengeSportEvent);
+		}
+
 		return "redirect:/challengeSportEvent/show?challengeId=" + challengeId;
 	}
-	
+
 	@RequestMapping(value = "/challengeSportEvent/edit", method = RequestMethod.GET)
-	public String edit(@RequestParam("id") int challengeSportEventId, Model model, Principal principal) {
+	public String edit(@RequestParam("id") int challengeSportEventId,
+			Model model, Principal principal) {
 		logger.debug("Received request to show list challenge's sportevents page");
-		
+
 		User currentUser = userService.getUserByUsername(principal.getName());
-		ChallengeSportEvent challengeSportEvent = challengeSportEventService.getById(challengeSportEventId);
-		
-		
+		ChallengeSportEvent challengeSportEvent = challengeSportEventService
+				.getById(challengeSportEventId);
+
+		if (!currentUser.getId().equals(
+				challengeSportEvent.getChallenge().getOwner().getId())) {
+			// Attempted to edit wrong challenge data
+			return "redirect:/denied";
+		}
+
 		model.addAttribute("challengeSportEventInstance", challengeSportEvent);
 		model.addAttribute("pointFactorTypeEnum", PointFactorType.values());
 		model.addAttribute("loggedInUser", currentUser);
-		
+
 		return "challengeSportEvent/edit";
 	}
-	
+
 	@RequestMapping(value = "/challengeSportEvent/edit", method = RequestMethod.POST)
 	public String updateChallenge(
 			@ModelAttribute("challengeSportEventInstance") ChallengeSportEvent challengeSportEvent,
-			Model model) throws ParseException {
-			logger.debug("Received request to update challengeSportEvent");
-			
+			Model model, Principal principal) throws ParseException {
+		logger.debug("Received request to update challengeSportEvent");
+
+		User currentUser = userService.getUserByUsername(principal.getName());
+		ChallengeSportEvent oldChallengeSportEvent = challengeSportEventService
+				.getById(challengeSportEvent.getId());
+
+		if (!currentUser.getId().equals(
+				oldChallengeSportEvent.getChallenge().getOwner().getId())) {
+			// Attempted to edit wrong challenge data
+			return "redirect:/denied";
+		}
+
 		ChallengeSportEvent editedSportEvent = challengeSportEventService
 				.setNewPropertiesToExistingChallenge(challengeSportEvent);
-//		if (challengeSportEventService.isValid(editedSportEvent)) {
-			challengeSportEventService.save(editedSportEvent);
-			return "redirect:/challengeSportEvent/show?challengeId="
-					+ editedSportEvent.getChallenge().getId();
-//		} else {
-//			setupErrorModel(model, challenge);
-//			return "challengeSportEvent/show";
-//		}
+		challengeSportEventService.save(editedSportEvent);
+		return "redirect:/challengeSportEvent/show?challengeId="
+				+ editedSportEvent.getChallenge().getId();
 	}
 
-	
-	
 }
